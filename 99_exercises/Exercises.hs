46ac0f9d34d00fb3185b694466a33bac72c0c833
maximg <maxim.golov@gmail.com>
2015-11-30 23:19:30 +0100

    Solved problems 5..7

--- 99_exercises/Exercises.hs
+++ 99_exercises/Exercises.hs
@@ -8,6 +8,25 @@ myLast = head . reverse
 myButLast = head . tail . reverse
 
 -- 3. find nth element of a list, starting with 1
---elementAt    :: [a] -> Int -> a
 elementAt x k | k < 1 = error "Index out of bounds"
               | otherwise = (head . drop (k - 1)) x 
+
+-- 4. find the number of elements of a list
+myLength :: [a] -> Int
+myLength [] = 0
+myLength (_:xs) = myLength xs + 1
+
+-- 5. reverse a list
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+-- 6. check if a list is a palindrome
+isPalindrome x = x == reverse x
+
+-- 7. flatten a nested list
+data NestedList a = Elem a | List [NestedList a]
+flatten              :: NestedList a -> [a]
+flatten (Elem x)      = [x]
+flatten (List [])     = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
